af4a2e81ab193f9bbe04415506ac25b75e4fc8ee
Bo Cao <caobo.86.mail@gmail.com>
2023-06-24 22:30:49 +0900

    tweaks

--- open_xplr.sh
+++ open_xplr.sh
@@ -2,4 +2,5 @@
 
 # Use xplr to open a file or directory in the default app
 TARGET=$(xplr $1)
-[ -z "$TARGET" ] || ( $(file -b $TARGET | rg -q 'text|empty') && nvim $TARGET || open "$TARGET")
+[ -z "$TARGET" ] || ( $(file -b $TARGET | rg -q 'text|empty') && nvim $TARGET ||
+    ( [ "$(uname)" = "Darwin" ] && open "$TARGET" ) )
