a0ab2bbf8bea3c8aa0b5ce20abb9c0ec717af0a9
Bo Cao <caobo.86.mail@gmail.com>
2023-06-24 21:36:26 +0900

    tweaks

--- link_script.sh
+++ link_script.sh
@@ -5,5 +5,6 @@ link_path="$HOME/.local/bin"
 mkdir -p "$link_path"
 
 fd ".*\.sh" "$file_path" -d 1 -t f -x basename {} .sh \; | while IFS= read -r file; do
-    ln -s "$file_path/$file.sh" "$link_path/$file"
+    chmod u+x "$file_path/$file.sh"
+    ln -sf "$file_path/$file.sh" "$link_path/$file"
 done
