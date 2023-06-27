f7aa86b009df3f70025c235c2eb79c7a63da52e5
Bo Cao <caobo.86.mail@gmail.com>
2023-06-27 21:27:59 +0900

    cleanup.

--- upfile.sh
+++ upfile.sh
@@ -2,9 +2,10 @@
 
 full_name="$1"
 file_name="$(echo "$full_name" | awk -F '/' '{print $NF}')"
-if [ "$(uname)" = "Linux" ]; then
-    curl --upload-file "$full_name" https://transfer.sh/"$file_name" | wl-copy && echo "Ready to share."
-fi
-if [ "$(uname)" = "Darwin" ]; then
-    curl --upload-file "$full_name" https://transfer.sh/"$file_name" | pbcopy && echo "Ready to share."
-fi
+
+case "$(uname)" in
+    Linux)
+        curl --upload-file "$full_name" https://transfer.sh/"$file_name" | wl-copy && echo "Ready to share.";;
+    Darwin)
+        curl --upload-file "$full_name" https://transfer.sh/"$file_name" | pbcopy && echo "Ready to share.";;
+esac
