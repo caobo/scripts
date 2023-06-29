e299ba7a2ada6a5186b5be99ae43d24f02832e67
Bo Cao <caobo.86.mail@gmail.com>
2023-06-29 21:01:31 +0900

    More minor tweaks.

--- clean_user_tmp.sh
+++ clean_user_tmp.sh
@@ -10,7 +10,7 @@ if [ ! -f "$HOME/tmp/log/clean_list_$log_time" ]; then
     # Make a list for files that to be cleaned
     file="$(fd ".*" "$HOME/tmp" -d 1 --change-older-than 7d)"
     # Create a clean list file
-    printf "$file" > "$HOME/tmp/log/clean_list_$log_time"
+    printf "$file\n" > "$HOME/tmp/log/clean_list_$log_time"
     # Cleanup file listed in the clean_list
     if [ -n "$file" ]; then
         while IFS= read -r i; do
@@ -22,7 +22,7 @@ fi
 # Find the old log file and clean it
 log_file="$(fd '.*' "$HOME/tmp/log" --change-older-than 7d)"
 if [ -n "$log_file" ]; then
-    printf "$log_file" > "$HOME/tmp/clean_list_for_log"
+    printf "$log_file\n" > "$HOME/tmp/clean_list_for_log"
     while IFS= read -r i; do
         rm "$i"
     done < "$HOME/tmp/clean_list_for_log"
