023fb71b65682e754705a1cc09b84e970f5449e7
Bo Cao <caobo.86.mail@gmail.com>
2023-06-29 16:16:17 +0900

    fine tweaks.

--- auto_update.sh
+++ auto_update.sh
@@ -6,32 +6,25 @@ RESET='\033[0m'
 
 # Update Homebrew
 brew update > /dev/null &&
-    ( ([ -z "$(brew outdated)" ] && echo "${BLUE}Homebrew formulas are up to date${RESET}") ||
+    ( ([ -z "$(brew outdated)" ] && printf "${BLUE}Homebrew formulas are up to date${RESET}\n") ||
     ( brew upgrade && brew cleanup )
-) || echo "${BLUE}Homebrew formulas are updated${RESET}"
+) || printf "${BLUE}Homebrew formulas are updated${RESET}\n"
 
 # Update texlive packages
 if [ $? -eq 0 ]; then
     sudo tlmgr update --self --all &&
-    echo "${BLUE}Texlive packages are updated${RESET}"
+    printf "${BLUE}Texlive packages are updated${RESET}\n"
 fi
 
 # Update Oh My Zsh and Python packages when "all" argument is provided
 if [ $? -eq 0 ]; then
     if [ "$1" = "all" ]; then
-        pip-review --auto  && echo  "${BLUE}Python packages are updated${RESET}"
+        pip-review --auto  && printf  "${BLUE}Python packages are updated${RESET}\n"
     fi
 fi
 
 # Print result
 # Function to repeat a character a certain number of times
-repeatf() {
-    local char=$1
-    local count=$2
-    printf "%*s" "$count" | tr ' ' "$char"
-}
-repeatf "-" | tr -d '\n'
-echo
-echo "${BLUE}All packages are up to date${RESET}"
-repeatf "-" | tr -d '\n'
-echo
+printf "+-------------------------------------+\n"
+printf "  ${BLUE}All packages are up to date${RESET}   \n"
+printf "+-------------------------------------+\n"
--- clean_user_tmp.sh
+++ clean_user_tmp.sh
@@ -10,21 +10,21 @@ if [ ! -f "$HOME/tmp/log/clean_list_$log_time" ]; then
     # Make a list for files that to be cleaned
     file="$(fd ".*" "$HOME/tmp" -d 1 --change-older-than 7d)"
     # Create a clean list file
-    echo "$file" > "$HOME/tmp/log/clean_list_$log_time"
+    printf "$file" > "$HOME/tmp/log/clean_list_$log_time"
     # Cleanup file listed in the clean_list
     if [ -n "$file" ]; then
-    while IFS= read -r i; do
-        rm -rdf "$i"
-    done < "$HOME/tmp/log/clean_list_$log_time"
+        while IFS= read -r i; do
+            rm -rd "$i"
+        done < "$HOME/tmp/log/clean_list_$log_time"
     fi
 fi
 
 # Find the old log file and clean it
 log_file="$(fd '.*' "$HOME/tmp/log" --change-older-than 7d)"
 if [ -n "$log_file" ]; then
-echo "$log_file" > "$HOME/tmp/clean_list_for_log"
-while IFS= read -r i; do
-    rm "$i"
-done < "$HOME/tmp/clean_list_for_log"
-rm "$HOME/tmp/clean_list_for_log"
+    printf "$log_file" > "$HOME/tmp/clean_list_for_log"
+    while IFS= read -r i; do
+        rm "$i"
+    done < "$HOME/tmp/clean_list_for_log"
+    rm "$HOME/tmp/clean_list_for_log"
 fi
