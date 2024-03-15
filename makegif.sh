ce70087913af2f8ce727f6cb8caf7f3b1bd111eb
Bo Cao <caobo.86.mail@gmail.com>
2024-03-15 12:36:17 +0800

    reduce gif file size

--- makegif.sh
+++ makegif.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
 outdir="$HOME/tmp"
-ffmpeg -i "$1" -vf "fps=15,scale=320:-1:flags=lanczos" -c:v gif -f gif "$outdir/$2.gif"
+ffmpeg -i "$1" -vf "fps=10,scale=320:-1:flags=lanczos" -qscale:v 12 -c:v gif -f gif "$outdir/$2.gif"
