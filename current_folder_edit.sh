7d62e5c91a08904cfc642aee7e444378e9fe164b
Bo Cao <caobo.86.mail@gmail.com>
2024-04-09 19:33:35 +0800

    modify file search script

--- current_folder_edit.sh
+++ current_folder_edit.sh
@@ -3,5 +3,7 @@
 # Edit selected file in the current folder
 FILE=$(fd '.*' $(pwd) -t f -x file --mime-type |
     awk -F ':' '/.*:.*text|empty/ { print $1}' |
-    fzf -d '/' --with-nth='-2','-1' --height=~100% --cycle --preview 'bat --color=always {}' --info=inline --border=rounded)
+    fzf -d '/' --with-nth='-2','-1' --height=~100% --cycle --preview 'bat --color=always {}'\
+    --info=inline --border=rounded --header="Edit files in current directory" --header-first\
+    --prompt="Searching >_ ")
     [ -z "$FILE" ] && echo 'Please select a file.' || nvim "$FILE"
