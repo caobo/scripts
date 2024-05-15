e45d9e95d0126a4c1b73372556fc636e3af9e83c
Bo Cao <caobo.86.mail@gmail.com>
2024-05-15 10:10:20 +0800

    improve: adding exit in adgt and rmgt.

--- adgt.sh
+++ adgt.sh
@@ -3,4 +3,13 @@
 # Create a goto directory
 GT_LIST="$HOME/.config/zsh/gt_list"
 [ -d "$HOME/.config/zsh" ] || mkdir "$HOME/.config/zsh"
-[ -z "$@" ] && echo "$(pwd)" >> "$GT_LIST" || echo "$@" >> "$GT_LIST"
+
+[ -z "$@" ] && GT_ITEM="$(pwd)" || GT_ITEM="$@"
+
+if grep -q "$GT_ITEM" "$GT_LIST"; then
+    echo "$GT_ITEM is already there ^_^"
+    exit 0
+fi
+
+echo "$GT_ITEM" >> "$GT_LIST"
+echo "$GT_ITEM is added to the goto list."
--- rmgt.sh
+++ rmgt.sh
@@ -5,11 +5,16 @@ GT_LIST="$HOME/.config/zsh/gt_list"
 LINE=$(cat "$GT_LIST" |\
     fzf-tmux -p50% --cycle --header="Select a goto directory from the list to delete" --header-first\
     --prompt="Selecting >_ ")
-[ -z "$LINE" ] && echo "Please select a goto directory to delete." || 
+
+[ -z "$LINE" ] && (echo "Please select a goto directory to delete." && exit 0) || 
 (
 if [ "$(uname)" = "Linux" ]; then
     sed -i "\|"$LINE"$|d" "$GT_LIST"
+    echo "$LINE has been deleted from goto list."
+    exit0
 elif [ "$(uname)" = "Darwin" ]; then
     sed -i '' "\|"$LINE"$|d" "$GT_LIST"
+    echo "$LINE has been deleted from goto list."
+    exit 0
 fi
 )
