fcd99e03fdb355c2ea8ca9371d4a69618d1bf30b
Bo Cao <caobo.86.mail@gmail.com>
2024-05-20 09:37:29 +0800

    improve: check if a given directory is a valid path name.

--- adgt.sh
+++ adgt.sh
@@ -6,8 +6,15 @@ GT_LIST="$HOME/.config/zsh/gt_list"
 
 [ -z "$@" ] && GT_ITEM="$(pwd)" || GT_ITEM="$@"
 
+# Cheak if the item already existed in the list
 if grep -q "$GT_ITEM$" "$GT_LIST"; then
     echo "$GT_ITEM is already there ^_^"
+    exit 0
+fi
+
+# Cheak if the item is a valid directory
+if [ ! -d "$GT_ITEM" ]; then
+    echo "Oops, $GT_ITEM is not a valid directory."
     exit 0
 fi
 
