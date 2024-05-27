08d4b712f2888393a501cf637cb49b4bf4df770b
Bo Cao <caobo.86.mail@gmail.com>
2024-05-27 13:08:50 +0800

    improve: use xargs command

--- vp.sh
+++ vp.sh
@@ -2,7 +2,7 @@
 
 [ -n "$1" ] && dirname="$1" || dirname="$(pwd)"
 
-filename="$( fd . "$dirname" --max-depth 2 -t f -x file --mime-type | awk -F ":" ' /video/ {print $1}' |\
+filename="$( fd . "$dirname" --max-depth 2 -t f | xargs -P8 -I{} file --mime-type {} | awk -F ":" ' /video/ {print $1}' |\
     fzf-tmux -p50% --reverse --delimiter "/" --with-nth='-2','-1' --header="Play video" --header-first --prompt="Searching video >_ ")"
 
 if [ -n "$filename" ]; then
