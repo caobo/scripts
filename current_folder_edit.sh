a6c4e8cd3a45b1548c1f52b73cb9c71f39427706
Bo Cao <caobo.86.mail@gmail.com>
2024-05-27 13:09:24 +0800

    fix: typo

--- current_folder_edit.sh
+++ current_folder_edit.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Edit selected file in the current folder
-FILE=$(fd '.*' $(pwd) -t f |xargs -P8 -I{} file --mime-type {} |
+FILE=$(fd '.*' $(pwd) -t f | xargs -P8 -I{} file --mime-type {} |
     awk -F ':' '/.*:.*text|empty/ { print $1}' |
     fzf-tmux -p60% --reverse --delimiter '/' --with-nth='-2','-1' --cycle --preview 'bat --color=always {}'\
     --header="Edit files in current directory" --header-first\
