fb80225fd5841e88c2047a6ac6d35d1d06dad3f6
Bo Cao <caobo.86.mail@gmail.com>
2025-02-11 11:18:50 +0800

    fix: typo

--- getsize.sh
+++ getsize.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-[ -d "$1" ] && du -sh "$1" | awk '{print "folder size is: " $1}' || du -sh "$1" | awk '{print "file size is:" $1}'
+[ -d "$1" ] && du -sh "$1" | awk '{print "folder size is: " $1}' || du -sh "$1" | awk '{print "file size is: " $1}'
