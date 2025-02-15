73f15ee310e1a86e8c0f428382a4c28f8727269e
Bo Cao <caobo.86.mail@gmail.com>
2025-02-15 12:53:15 +0800

    Modify: improve the use of tr command

--- spell.sh
+++ spell.sh
@@ -1,14 +1,27 @@
 #!/bin/sh
 
 # Spell check function
-if [ "$(uname)" = "Darwin" ]; then
+main(){
+
+osname="$(uname)"
+
+if [ $osname = "Darwin" ]; then
     [ -n "$1" ] && WORD="$1" || WORD=$(pbpaste)
-elif [ "$(uname)" = "Linux" ]; then
+elif [ $osname = "Linux" ]; then
     [ -n "$1" ] && WORD="$1"|| WORD=$(wl-paste|less)
 fi
-CSPELL=$(echo "$WORD" | aspell pipe | awk -F ':' '{print $2}' | tr ',' '\n' | fzf --height=~50% --layout reverse-list)
-if [ "$(uname)" = "Darwin" ]; then
+
+CSPELL=$(echo "$WORD" | aspell pipe | awk -F ':' '{print $2}' |\
+    tr -cs '[:alpha:]' '\n'|\
+    fzf --height=~50% --layout reverse-list)
+if [ $osname = "Darwin" ]; then
     [ -n "$CSPELL" ] && echo "$CSPELL" | pbcopy
-elif [ "$(uname)" = "Linux" ]; then
+    return 0
+elif [ $osname = "Linux" ]; then
     [ -n "$CSPELL" ] && echo "$CSPELL" | wl-copy
+    return 0
 fi
+
+return 1
+}
+main "$@"
