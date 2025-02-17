1b5dea4a5884929651f490ccae2c6747fea3c605
Bo Cao <caobo.86.mail@gmail.com>
2025-02-17 10:07:50 +0800

    Add: add preview in git show diff script

--- showdiff.sh
+++ showdiff.sh
@@ -1,5 +1,7 @@
 #/usr/bin/env dash
 
-COMMIT=$(git log --pretty=format:"%h - %s (%ar)" -10 | fzf --header-first --header="Choose a git commit"| awk '{print $1}')
+COMMIT=$(git log --pretty=format:"%h - %s (%ar)" -10 | \
+    fzf --header-first --header="Choose a git commit" --preview 'git show --color=always $(echo {} | awk "{print \$1}")'| \
+    awk '{print $1}')
 
 git show $COMMIT
