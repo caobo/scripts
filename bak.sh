93a8f59b3852b698544fd2588661facc3f4c8524
Bo Cao <caobo.86.mail@gmail.com>
2025-02-22 11:19:47 +0800

    Modify: in bak script: rsync options: use --progress and --partial (combined as -P)

--- bak.sh
+++ bak.sh
@@ -3,5 +3,5 @@
 if ! [ -d "/Volumes/CBSD" ]; then
     echo "Please plug in the backup disk."
 else
-    rsync -avzh --progress --stats "$HOME/Documents" "/Volumes/CBSD/"
+    rsync -avzhP --stats "$HOME/Documents" "/Volumes/CBSD/"
 fi
