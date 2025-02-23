0d4b04e03e44b4e7b1930b07605dd0699bab1585
Bo Cao <caobo.86.mail@gmail.com>
2025-02-23 10:50:09 +0800

    Modify: rename: gcdiff->gcshow; modify: git show preview using --stat option

---
+++ gcshow.sh
@@ -0,0 +1,14 @@
+#/usr/bin/env dash
+
+main() {
+local COMMIT=$(git log --pretty=format:"%h - %s (%ar)" -10 | \
+    fzf --header-first --header="Choose a git commit" --preview 'git show --stat --color=always $(echo {} | awk "{print \$1}")'| \
+    awk '{print $1}')
+if [ "$1" = "wd" ]; then
+    git show --word-diff $COMMIT
+    exit 0;
+fi
+    git show $COMMIT
+}
+
+main "$@"
