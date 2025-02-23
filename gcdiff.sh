598e4763948c5a673bfade26e0aeb7e6a6d8089c
Bo Cao <caobo.86.mail@gmail.com>
2025-02-23 10:39:33 +0800

    Add: show diff by word function

--- gcdiff.sh
+++ gcdiff.sh
@@ -1,7 +1,14 @@
 #/usr/bin/env dash
 
+main() {
 COMMIT=$(git log --pretty=format:"%h - %s (%ar)" -10 | \
     fzf --header-first --header="Choose a git commit" --preview 'git show --color=always $(echo {} | awk "{print \$1}")'| \
     awk '{print $1}')
+if [ "$1" = "wd" ]; then
+    git show --word-diff $COMMIT
+    exit 0;
+fi
+    git show $COMMIT
+}
 
-git show $COMMIT
+main "$@"
