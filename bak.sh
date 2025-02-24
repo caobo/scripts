3fa7e60287277f02ce2080f262be27eab2b4b6e4
Bo Cao <caobo.86.mail@gmail.com>
2025-02-24 15:56:34 +0800

    Modify: only backup once a day.

--- bak.sh
+++ bak.sh
@@ -7,7 +7,11 @@ if ! [ -d "/Volumes/CBSD/" ]; then
     exit 1
 fi
 
+if grep -q "$DATE" "$BAK_DIR/bak.log"; then
+    exit 0
+fi
+
 rsync -avzhP --stats --exclude='*\.git' "$HOME/Documents" "$BAK_DIR"
 
 touch "$BAK_DIR/bak.log"
-echo "$DATE" >> "$BAK_DIR/bak.log"
+echo "on $DATE: backup completed" >> "$BAK_DIR/bak.log"
