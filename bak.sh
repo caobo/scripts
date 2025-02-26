3b292f966ed9083249017e0cb9f5d1aa34ff9327
Bo Cao <caobo.86.mail@gmail.com>
2025-02-26 11:24:12 +0800

    Modify: give the full path of diskutil command

--- bak.sh
+++ bak.sh
@@ -3,13 +3,14 @@
 DATE="$(date +"%F")"
 BAK_DIR="/Volumes/CBSD/bak"
 MOUNT_POINT="/Volumes/CBSD"
+DISKUTIL="/usr/sbin/diskutil"
 
 if ! [ -d "$MOUNT_POINT" ]; then
     exit 0
 fi
 
 if grep -q "$DATE" "$BAK_DIR/bak.log" 2>/dev/null; then
-    diskutil unmount "$MOUNT_POINT"
+    $DISKUTIL unmount "$MOUNT_POINT"
     exit 0
 fi
 
