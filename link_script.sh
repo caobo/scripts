adbf7a03e1860213543e3255d337b686693fec63
Bo Cao <caobo.86.mail@gmail.com>
2025-02-27 11:24:41 +0800

    Modify: cleanup linked files before make new linkes

--- link_script.sh
+++ link_script.sh
@@ -4,6 +4,7 @@
 file_path="$HOME/Documents/Software/scripts"
 link_path="$HOME/.local/bin"
 mkdir -p "$link_path"
+rm "$link_path"/*
 
 fd ".*\.sh" "$file_path" -d 1 -t f -x basename {} .sh \; | while IFS= read -r file; do
     chmod u+x "$file_path/$file.sh"
