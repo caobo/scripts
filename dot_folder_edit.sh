ee687d34e52b6e9352bea9639fbfca05d61c2904
Bo Cao <caobo.86.mail@gmail.com>
2025-03-10 15:33:28 +0800

    Modify: improve robustness

--- dot_folder_edit.sh
+++ dot_folder_edit.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Edit selected file in the dot_files folder
-FILE=$(fd '.*' "$HOME/Documents/Software/dot_files" -t f | xargs -P8 -I{} file --mime-type {} |
+FILE=$(fd '.*' "$HOME/Documents/Software/dot_files" -t f | xargs -P10 -I{} file --mime-type {} |
     awk -F ':' '/.*:.*text|empty/ { print $1}' |
     fzf --delimiter '/' --with-nth='-2','-1' --cycle --preview 'bat --color=always {}'\
     --header="Edit config files" --header-first --prompt="Searching >_ ")
