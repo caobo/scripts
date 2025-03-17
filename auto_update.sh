2dcb8f9029258a0534bdf04b8dcc244947101176
Bo Cao <caobo.86.mail@gmail.com>
2025-03-17 15:52:27 +0800

    Remove: pip update

--- auto_update.sh
+++ auto_update.sh
@@ -11,17 +11,8 @@ brew update > /dev/null &&
 ) || printf "${BLUE}Homebrew formulas are updated${RESET}\n"
 
 # Update texlive packages
-if [ $? -eq 0 ]; then
-    sudo tlmgr update --self --all &&
-    printf "${BLUE}Texlive packages are updated${RESET}\n"
-fi
-
-# Update Oh My Zsh and Python packages when "all" argument is provided
-if [ $? -eq 0 ]; then
-    if [ "$1" = "all" ]; then
-        pip-review --auto  && printf  "${BLUE}Python packages are updated${RESET}\n"
-    fi
-fi
+sudo tlmgr update --self --all &&
+printf "${BLUE}Texlive packages are updated${RESET}\n"
 
 # Print result
 # Function to repeat a character a certain number of times
