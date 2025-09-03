7f0a17b2486f0c5bd3adba8d07b2c184cd26a9c2
Bo Cao <caobo.86.mail@gmail.com>
2025-09-03 13:29:27 +0800

    Modify: misc

--- bak.sh
+++ bak.sh
@@ -5,6 +5,9 @@ BAK_DIR="/Volumes/CBSD/bak"
 MOUNT_POINT="/Volumes/CBSD"
 DISKUTIL="$(which diskutil)"
 
+mkdir -p "$BAK_DIR"
+touch "$BAK_DIR/bak.log"
+
 if ! [ -d "$MOUNT_POINT" ]; then
     exit 0
 fi
@@ -14,8 +17,6 @@ if grep -q "$DATE" "$BAK_DIR/bak.log" 2>/dev/null; then
     exit 0
 fi
 
-mkdir -p "$BAK_DIR"
 if rsync -azhP --stats --exclude='.git/' "$HOME/Documents" "$BAK_DIR"; then
-    touch "$BAK_DIR/bak.log"
     echo "on $DATE: backup completed" >> "$BAK_DIR/bak.log"
 fi
